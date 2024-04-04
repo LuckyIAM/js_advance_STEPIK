fcd63fd42caff2c7d1bbd71ef3b1685ef27c0096
LuckyIAM <zavalisca.elena@gmail.com>
2024-04-04 09:59:27 +0300

    All sorts method is done

--- sorts_methods.js
+++ sorts_methods.js
@@ -134,3 +134,23 @@ let sortedArr = mergeSort(arr);
 console.log(sortedArr);
 
 // I can add on branch in git. now I have two baranch in git master and stepik
+
+function quickSort(arr){
+    if(arr.length <= 1){
+        return arr
+    }
+    let pivot = arr[0]
+    let left = []
+    let right = []
+
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+
+    return quickSort(left).concat(pivot, quickSort[right])
+
+}
